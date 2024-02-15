1aa8019998c1ec92b6a69e0081659a9346617d45
Damien Cuda <contact@damiencuda.com>
2024-02-15 20:29:12 +0100

    last commit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\ProductRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,11 +9,19 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+
+    #[Route('/product', name: 'app_product', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $productRepository->findAll()
+        ]);
+    }
+    #[Route('/product/{id}', name: 'app_product_details')]
+    public function singleProduct($id): Response
+    {
+        return $this->render('product/product_detail.html.twig', [
+            'product' => $productRepository->findOne($id)
         ]);
     }
 }
